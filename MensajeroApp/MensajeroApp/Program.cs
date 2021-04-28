f9a88c3d39f96efb4d8374f919de100ccaeca9b8
GabrielBaezB <gabooo@live.com>
2021-04-27 22:27:54 -0400

    Se creo el DAO y DAL

---
+++ MensajeroApp/MensajeroApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using MensajeroModel.DTO;
+using MensajeroModel.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroApp
+{
+    class Program
+    {
+        static IMensajeDAL dal = MensajeDALFactory.CreateDal();
+
+        static void Main(string[] args)
+        {
+            Mensaje m = new Mensaje()
+            {
+                Nombre = "Gabriel",
+                Detalle = "Estoy Volao",
+                Tipo = "Estado de coma",
+
+            };
+            dal.Save(m);
+        }
+    }
+}
---
+++ MensajeroApp/MensajeroModel/DAL/MensajeDALArchivo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MensajeroModel.DTO;
+
+namespace MensajeroModel.DAL
+{
+    public class MensajeDALArchivo : IMensajeDAL
+    {
+        // ruta donde guardara el archivo, se puede poner 
+        //directamente la ruta donde quiera guardar
+        private string archivo = Directory.GetCurrentDirectory()
+            + Path.DirectorySeparatorChar + "mensaje.csv";
+
+        //Patron Singleton
+        //1.Constructor privado
+        private MensajeDALArchivo()
+        {
+
+        }
+        //2. Un atributo de tipo estatico privado de la misma instancia
+        private static IMensajeDAL instancia;
+        //3.Un metodo estatico que permita obtener la unica instancia
+        public static IMensajeDAL GetInstance()
+        {
+            if (instancia == null)
+            { instancia = new MensajeDALArchivo(); }
+                
+            return instancia;
+        }
+
+        public List<Mensaje> GetAll()
+        {
+            List<Mensaje> lista = new List<Mensaje>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string linea = null;
+                    do
+                    {
+                        if(linea != null)
+                        {
+                            string[] textoArray = linea.Split(';');
+                            Mensaje m = new Mensaje()
+                            {
+                                Nombre = textoArray[0],
+                                Detalle = textoArray[1],
+                                Tipo = textoArray[2]
+                            };
+                            lista.Add(m);
+                        }
+                    } while (linea != null);
+                }
+
+            }catch(IOException ex)
+            {
+                lista = null;
+            }
+            return lista;
+        }
+
+        public void Save(Mensaje m)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(m);
+                    writer.Flush();
+                }
+
+            }catch(IOException ex)
+            {
+
+            }
+        }
+    }
+}
